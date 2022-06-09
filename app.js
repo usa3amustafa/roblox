f815630ac42e86acf3927df55af4817b64e2706c
usa3amustafa <usa3a3ustafa@outlook.com>
2022-06-09 13:59:31 +0500

    firts

---
+++ app.js
@@ -0,0 +1,34 @@
+const hamburger = document.querySelector('.hamburger')
+const navbar = document.querySelector('.nav-items')
+const close = document.querySelector('.close')
+
+hamburger.addEventListener('click', () => {
+  navbar.classList.add('show-navbar')
+})
+
+close.addEventListener('click', () => {
+  navbar.classList.remove('show-navbar')
+})
+
+// counter
+const counters = document.querySelectorAll('.article-two-number')
+
+const speed = 300
+
+counters.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target')
+    const count = +counter.innerText
+
+    const inc = target / speed
+
+    if (count < target) {
+      counter.innerText = (count + inc).toFixed(3)
+      setTimeout(updateCount, 1)
+    } else {
+      counter.innerText = target
+    }
+  }
+
+  updateCount()
+})
